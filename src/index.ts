d46fdec8743c68e2c2d4b6cb2beb7547899c2ae3
Yuki Kokubo <yuukikokubo.pg@gmail.com>
2020-07-11 00:25:37 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,29 @@ import * as ts from "typescript";
 
 function astFactory(message: string) {
   return [
-    ts.createExpressionStatement(ts.createCall(
-      ts.createPropertyAccess(
-        ts.createIdentifier("console"),
-        ts.createIdentifier("log")
-      ),
-      undefined,
-      [ts.createStringLiteral(message)]
-    ))
+    ts.createExpressionStatement(
+      ts.createCall(
+        ts.createPropertyAccess(
+          ts.createIdentifier("console"),
+          ts.createIdentifier("log")
+        ),
+        undefined,
+        [ts.createStringLiteral(message)]
+      )
+    ),
   ];
 }
+
+const printer = ts.createPrinter();
+
+function main(message: string) {
+  const print = printer.printList(
+    ts.ListFormat.MultiLine,
+    ts.createNodeArray(astFactory(message)),
+    ts.createSourceFile("", "", ts.ScriptTarget.ES2015)
+  );
+
+  console.log(print);
+}
+
+main(process.argv[2].slice(0));
